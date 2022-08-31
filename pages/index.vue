68ddc4ccd8ca8a46582232bbc7b6d63c8172fa33
Chukwudi Egbuna <chukwudiegbuna@Chukwudis-MacBook-Pro.local>
2022-08-31 10:56:28 +0100

    portfolio styling

--- pages/index.vue
+++ pages/index.vue
@@ -1,47 +1,62 @@
 <template>
      <header class="full-page">
-        <div className='nav-bar'>
+        <div class='nav-bar'>
           <div class="logo-container">
-            <img src="assets/icons/sa.svg" alt="logo">
+            <img src="~/assets/icons/sa.svg" alt="logo">
           </div>
-            <a href="">Resume</a>
-            <a href="">Project</a>
-            <a href="">About</a>
-            <button class="btn-stroke">Contact Me</button>
+            <div class="nav-nenu">
+            <ul>
+                <li><a href="">Resume</a></li>
+                <li><a href="">Project</a></li>
+                <li><a href="">About</a></li>
+                <li><button class="btn-stroke">Contact Me</button></li>
+            </ul>    
+            </div>
         </div>
 
           <div class="first-container">
             <div class="hero-section">
-            <div class="web-block">Samson Akisanya <br> Senior <span class="web-word">Android </span> Developer and <br> <span class="web-word-two">Kotlin Backend</span> Developer</div>
+            <div class="web-block"> <span class="web-words"> Samson Akisanya </span> <br> Senior <span class="web-word">Android </span> Developer and <br> <span class="web-word-two">Kotlin Backend</span> Developer</div>
             </div >
           <div class="social-media">
-                    <img src="assets/icons/twitter.svg" alt="logo-social">
-                    <img src="assets/icons/github.svg" alt="logo-social">
-                    <img src="assets/icons/linkedin.svg" alt="logo-social">
-                    <img src="assets/icons/instagram.svg" alt="logo-social">
+                    <img src="~/assets/icons/twitter.svg" alt="logo-social" class="s-media">
+                    <img src="~/assets/icons/github.svg" alt="logo-social" class="s-media">
+                    <img src="~/assets/icons/linkedin.svg" alt="logo-social" class="s-media">
+                    <img src="~/assets/icons/instagram.svg" alt="logo-social" class="s-media">
+          </div>
 
-            <button class="btn-filled"> <img src="assets/icons/calender.svg" alt="btn-call"> Schedule a call </button>
-            </div>
+                    <div class="button">   
+                          <button class="btn-filled"> <img src="assets/icons/calender.svg" alt="btn-call"> Schedule a call </button>
+                          <button class="btn-stroke"> <img src="assets/icons/calender.svg" alt="btn-call"> Hire me</button>
+                    </div>   
 
-            <div class="second container">
-                  <h5 class="extras"> 15+ Years of experience</h5>
-                   <h5 class="extras"> 100+ Completed projects</h5>
-                   <h5 class="extras"> 100+ Happy customers</h5>
+
+            <div class="second-cont">
+                <div class="second-container">
+                      <h5 class="extras"> 15+ Years of experience</h5>
+                      <h5 class="extras"> 100+ Completed projects</h5>
+                      <h5 class="extras"> 100+ Happy customers</h5>
+                </div>
             </div>
           </div>
 
 
           <div class="third-container">
              
+             <h3 class="texts">About Me</h3>
+              
               <section class="about-me">
-                  
-                  <img src="assets/images/BG.png" alt="image">
+            
+                 <div class="images">
+                      <img src="~/assets/images/BG.png" alt="image">
+                  </div>
+
                   <h6 class="text-area">Samson is a highly motivated professional, committed to achieving <br> excellence both professionally and personally. He has detailed knowledge <br>
                   and experience of Testing, Analysis, Design, Configuration, Development and <br> Implementation. Samson is a lateral thinker and has a strong passion for <br>
                   learning about new technologies. Samson has good communication skills <br> combined with the ability to relate well to people at all levels. Samson is a <br> 
                   great self starter and very capable of working independently. Samson has a <br> lot of experience working in an Agile scrum environment using JIRA. Samson <br>
                   recognizes the importance of Test Driven Development with Unit Testing and <br> is a diligent practitioner. Samson is an enthusiastic student of Machine <br> Learning and Data Science. <br>
-                  <code> I build </code> </h6>
+                  </h6>
 
               </section>
           </div>
@@ -51,14 +66,16 @@
               <section class="experience">
 
                     <h3 class="text">I've worked at</h3>
-                    <img src="" alt="assets/icons/VEON Logo.svg">
-                    <img src="" alt="assets/icons/skylogo.svg">
-                    <img src="" alt="assets/icons/yeologo.svg">
-                    <img src="" alt="assets/icons/dopaylogo.svg">
-                    <img src="" alt="assets/icons/isobarlogo.svg">
-                    <img src="" alt="assets/icons/Accedo Logo.svg">
-                    <img src="" alt="assets/icons/Nokia Logo.svg">
-                    <img src="" alt="assets/icons/clydesdale.svg">
+                <div class="logos">
+                    <img src="~/assets/icons/VEON_Logo.svg" alt="logo">
+                    <img src="~/assets/icons/skylogo.svg" alt="logo">
+                    <img src="~/assets/icons/yeologo.svg" alt="logo">
+                    <img src="~/assets/icons/dopaylogo.svg" alt="logo">
+                    <img src="~/assets/icons/isobarlogo.svg" alt="logo">
+                    <img src="~/assets/icons/Accedo Logo.svg" alt="logo">
+                    <img src="~assets/icons/Nokia_Logo.svg" alt="logo">
+                    <img src="~/assets/icons/clydesdale.svg" alt="logo">
+                </div>    
               
               </section>
               
@@ -67,47 +84,67 @@
           <div class="fifth-container">
 
               <section class="projects-box">
+
+                        <h3 class="texts">Projects i've worked on</h3>
+
+                <div class="project-A">
                        <div class="projects">
                                 <h4 class="project-name">MyPinPad</h4>
-                                <img src="assets/images/pinpad.png" alt="project-image">
+                                <img src="~/assets/images/pinpad.png" alt="project-image">
+                              <div class="role-date">
                                 <h4 class="project-role">Senior Andriod Dev</h4>
                                 <h4 class="project-date">Nov 2021-present</h4>
+                              </div>  
                        </div>
 
                        <div class="projects">
                                 <h4 class="project-name">Deliveroo</h4>
-                                <img src="assets/images/deliveroo.png" alt="project-image">
+                                <img src="~/assets/images/deliveroo.png" alt="project-image">
+                              <div class="role-date">
                                 <h4 class="project-role">Senior Andriod Dev</h4>
                                 <h4 class="project-date">Feb 2021-May 2021</h4>
+                              </div>   
                        </div>
 
                        <div class="projects">
                                 <h4 class="project-name">Curry Pc World Apps</h4>
-                                <img src="assets/images/currypc.png" alt="project-image">
+                                <img src="~/assets/images/currypc.png" alt="project-image">
+                              <div class="role-date">
                                 <h4 class="project-role">Senior Andriod Dev</h4>
                                 <h4 class="project-date">Dec 2020-Feb 2021</h4>
+                              </div>   
                        </div>
-
+              </div>
+                       
+                       
+              <div class="project-A">        
                        <div class="projects">
                                 <h4 class="project-name">Dopay</h4>
-                                <img src="assets/images/dopay.png" alt="project-image">
+                                <img src="~/assets/images/dopay.png" alt="project-image">
+                              <div class="role-date">
                                 <h4 class="project-role">Lead Andriod Dev</h4>
                                 <h4 class="project-date">2017</h4>
+                              </div>  
                        </div>
 
                        <div class="projects">
                                 <h4 class="project-name">Zara Med</h4>
-                                <img src="assets/images/zaramed.png" alt="project-image">
+                                <img src="~/assets/images/zaramed.png" alt="project-image">
+                              <div class="role-date">
                                 <h4 class="project-role">Lead Andriod Dev</h4>
                                 <h4 class="project-date">2020</h4>
+                              </div>  
                        </div>
 
                        <div class="projects">
                                 <h4 class="project-name">Weezy</h4>
-                                <img src="assets/images/weezy.png" alt="project-image">
+                                <img src="~/assets/images/weezy.png" alt="project-image">
+                              <div class="role-date">
                                 <h4 class="project-role">Lead Andriod Dev</h4>
                                 <h4 class="project-date">2020</h4>
+                              </div>  
                        </div>
+              </div> 
 
                        <div class="view-more">
                           View More Projects
@@ -117,7 +154,69 @@
           
           </div>
 
+
+          <footer class="footer">
+
+          <div class="footer-contact">
+             <div class="footer-text">
+                <h2>Get in touch</h2>
+             </div>
+
+             <div class="footer-text-area">
+                <h4>Hey there, would you like to take your project to the next level? <br> Get Samson on board to deliver excellent mobile solutions </h4>
+             </div>
+
+             <div class="footer-button">   
+                          <button class="btn-filled"> <img src="assets/icons/calender.svg" alt="btn-call"> Schedule a call </button>
+                          <!-- <button class="btn-stroke"> <img src="assets/icons/calender.svg" alt="btn-call"> Hire me</button> -->
+                    </div>   
+
+             <div class="connect">
+                <h5>Connect with me</h5>
+             </div> 
+
+              <div class="number">
+                <h5>+44 (0) 7401226202</h5>
+             </div> 
+
+             <div class="email">
+                <h5>info@mobileparadigm.co.uk</h5>
+             </div>
+
+             <div class="logo-footer">
+                    <img src="~/assets/icons/twit.svg" alt="logo" class="footer-logo">
+                    <img src="~/assets/icons/git.svg" alt="logo" class="footer-logo">
+                    <img src="~/assets/icons/link.svg" alt="logo" class="footer-logo">
+            </div>     
+
+          </div>
+
+          <div class="footer-form">
+             
+               <form action="" method="post">
+                  <div class="input-container">
+                    <input type="text" name="FullName:" id="FullName:" class="FullName:" placeholder="FullName:" required>
+                  </div>
+
+                  <div class="input-container">
+                    <input type="text" name="Email:" id="Email:" class="Email:" placeholder="Email:" required>
+                  </div>
+
+                  <div class="input-container-one">
+                    <input type="text" name="Message:" id="Message:" class="Message:" placeholder="Message:" required>
+                  </div>
+
+              </form>    
+          
+          </div>
+
+             
+          
+          </footer>
+
      </header>
+
+     
 </template>
 
 <script lang="ts">
